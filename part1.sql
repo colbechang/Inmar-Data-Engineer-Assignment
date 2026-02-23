73162fdbb8d38e20b4449d930b3f2477a7bace93
Colbe Chang <colbehchang@gmail.com>
2026-02-22 23:19:40 -0500

    Wrote query for part1 + answered questions

---
+++ part1.sql
@@ -0,0 +1,17 @@
+WITH mostRecentRedemption AS (
+  SELECT r.retailerName, rbd.redemptionDate, rbd.redemptionCount,  rbd.createDateTime,
+  COUNT(rbd.redemptionDate) OVER (PARTITION BY rbd.redemptionDate) AS numRecords,
+  ROW_NUMBER() OVER (PARTITION BY rbd.redemptionDate ORDER BY rbd.createDateTime DESC) AS rn
+  FROM tblRedemptions-ByDay rbd JOIN tblRetailers r ON rbd.retailerId= r.id
+  WHERE rbd.redemptionDate BETWEEN '2023-10-30' AND '2023-11-05'
+  AND r.retailerName = 'ABC Store'
+)
+SELECT redemptionDate, redemptionCount, createdatetime,
+CASE WHEN numRecords = 0 THEN 'Missing Data'
+     WHEN numRecords > 1 THEN 'Multiple Records'
+     ELSE 'OK'
+END AS status
+FROM mostRecentRedemption
+WHERE rn = 1
+
+
